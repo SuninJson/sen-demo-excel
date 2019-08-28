0c58bf1339f5b148eacd17689b4aa37253c43bbe
Evan Huang <1538356302@qq.com>
2019-08-29 02:47:29 +0800

    补充Excel中手机号码的归属地

---
+++ src/main/java/sen/demo/excel/PhoneAttributionUtil.java
@@ -0,0 +1,10 @@
+package sen.demo.excel;
+
+/**
+ * @author Huang Sen
+ */
+public class PhoneAttributionUtil {
+    public static void main(String[] args) {
+        System.out.println(ExcelTool.getCity("15775916647"));
+    }
+}
